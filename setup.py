e2cf12a3be50aabae3fad78d4177114a7b564a16
1risk <>
2022-04-11 18:33:17 +0800

    Updata import

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-VERSION = '0.5.2'
+VERSION = '0.5.3'
 project_urls={
     "Documentation": "https://github.com/RuofengX/EasyPy",
     "Issue tracker": "https://github.com/RuofengX/EasyPy/issues"
@@ -14,5 +14,6 @@ setup(
     packages=find_packages(),
     zip_safe=False,
     install_requires=[
+        'rich'
     ]
 )
