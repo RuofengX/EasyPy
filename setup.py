1bc76bec4fe58cc30ec7d34528aac36efeca8083
1risk <>
2022-04-11 21:25:11 +0800

    Update import

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-VERSION = '0.5.4'
+VERSION = '0.6.0'
 project_urls={
     "Documentation": "https://github.com/RuofengX/EasyPy",
     "Issue tracker": "https://github.com/RuofengX/EasyPy/issues"
@@ -14,7 +14,6 @@ setup(
     packages=find_packages(),
     zip_safe=False,
     install_requires=[
-        'rich',
-        'colorlog'
+        'rich'
     ]
 )
